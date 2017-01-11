21005d751331d983dcce29e88883d57699997bd0
Yulong Yu <yuylong@acm.org>
2017-01-11 13:10:26 +0800

    Add execution process for Function SIN.

--- FigureItNow7/finExecFuncMath.cpp
+++ FigureItNow7/finExecFuncMath.cpp
@@ -1,15 +1,19 @@
 
 #include "finExecFunction.h"
 
+#include <qmath.h>
+
 #include "finExecVariable.h"
 #include "finExecEnvironment.h"
 #include "finExecMachine.h"
+#include "finExecFlowControl.h"
 
-//static finErrorCode _sysfunc_mat_add(finExecFunction *self, finExecEnvironment *env,
-//                                     finExecMachine *machine, finExecFlowControl *flowctl);
+static finErrorCode _sysfunc_sin(finExecFunction *self, finExecEnvironment *env,
+                                 finExecMachine *machine, finExecFlowControl *flowctl);
+
 
 static struct finExecSysFuncRegItem _finSysFuncMathList[] = {
-//    { QString("mat_add"), QString("mat1,mat2"), _sysfunc_mat_add },
+    { QString("sin"), QString("rad"), _sysfunc_sin },
 
     { QString(), QString(), NULL }
 };
@@ -19,3 +23,30 @@ finErrorCode finExecFunction::registSysFuncMath()
     return finExecFunction::registSysFuncFromArray(_finSysFuncMathList);
 }
 
+static finErrorCode _sysfunc_sin(finExecFunction *self, finExecEnvironment *env,
+                                 finExecMachine *machine, finExecFlowControl *flowctl)
+{
+    finExecVariable *rad, *retvar;
+
+    if ( self == NULL || env == NULL || machine == NULL || flowctl == NULL )
+        return finErrorCodeKits::FIN_EC_NULL_POINTER;
+
+    rad = env->findVariable("rad")->getLinkTarget();
+    if ( rad == NULL )
+        return finErrorCodeKits::FIN_EC_NOT_FOUND;
+    if ( rad->getType() != finExecVariable::FIN_VR_TYPE_NUMERIC )
+        return finErrorCodeKits::FIN_EC_INVALID_PARAM;
+
+    retvar = new finExecVariable();
+    if ( retvar == NULL )
+        return finErrorCodeKits::FIN_EC_OUT_OF_MEMORY;
+
+    retvar->setType(finExecVariable::FIN_VR_TYPE_NUMERIC);
+    retvar->setNumericValue(sin(rad->getNumericValue()));
+    retvar->setWriteProtected();
+    retvar->clearLeftValue();
+
+    flowctl->setFlowNext();
+    flowctl->setReturnVariable(retvar);
+    return finErrorCodeKits::FIN_EC_SUCCESS;
+}
