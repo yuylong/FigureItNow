ceabde1a3ad8dada3c672e2d5083c6410139439f
Yulong Yu <yuylong@acm.org>
2017-01-11 13:21:44 +0800

    Add execution process for Function COS.

--- FigureItNow7/finExecFuncMath.cpp
+++ FigureItNow7/finExecFuncMath.cpp
@@ -10,10 +10,13 @@
 
 static finErrorCode _sysfunc_sin(finExecFunction *self, finExecEnvironment *env,
                                  finExecMachine *machine, finExecFlowControl *flowctl);
+static finErrorCode _sysfunc_cos(finExecFunction *self, finExecEnvironment *env,
+                                 finExecMachine *machine, finExecFlowControl *flowctl);
 
 
 static struct finExecSysFuncRegItem _finSysFuncMathList[] = {
     { QString("sin"), QString("rad"), _sysfunc_sin },
+    { QString("cos"), QString("rad"), _sysfunc_cos },
 
     { QString(), QString(), NULL }
 };
@@ -43,6 +46,34 @@ static finErrorCode _sysfunc_sin(finExecFunction *self, finExecEnvironment *env,
 
     retvar->setType(finExecVariable::FIN_VR_TYPE_NUMERIC);
     retvar->setNumericValue(sin(rad->getNumericValue()));
+    retvar->setWriteProtected();
+    retvar->clearLeftValue();
+
+    flowctl->setFlowNext();
+    flowctl->setReturnVariable(retvar);
+    return finErrorCodeKits::FIN_EC_SUCCESS;
+}
+
+static finErrorCode _sysfunc_cos(finExecFunction *self, finExecEnvironment *env,
+                                 finExecMachine *machine, finExecFlowControl *flowctl)
+{
+    finExecVariable *rad, *retvar;
+
+    if ( self == NULL || env == NULL || machine == NULL || flowctl == NULL )
+        return finErrorCodeKits::FIN_EC_NULL_POINTER;
+
+    rad = env->findVariable("rad")->getLinkTarget();
+    if ( rad == NULL )
+        return finErrorCodeKits::FIN_EC_NOT_FOUND;
+    if ( rad->getType() != finExecVariable::FIN_VR_TYPE_NUMERIC )
+        return finErrorCodeKits::FIN_EC_INVALID_PARAM;
+
+    retvar = new finExecVariable();
+    if ( retvar == NULL )
+        return finErrorCodeKits::FIN_EC_OUT_OF_MEMORY;
+
+    retvar->setType(finExecVariable::FIN_VR_TYPE_NUMERIC);
+    retvar->setNumericValue(cos(rad->getNumericValue()));
     retvar->setWriteProtected();
     retvar->clearLeftValue();
 
