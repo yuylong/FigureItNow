b7e09b100d946f2670bdcf521638e540792be277
Yulong Yu <yuylong@acm.org>
2017-01-11 16:14:09 +0800

    Add RunFunction script function.

--- FigureItNow7/finExecFuncSystem.cpp
+++ FigureItNow7/finExecFuncSystem.cpp
@@ -9,7 +9,7 @@ static finErrorCode _sysfunc_run_function(finExecFunction *self, finExecEnvironm
                                           finExecMachine *machine, finExecFlowControl *flowctl);
 
 static struct finExecSysFuncRegItem _finSysFuncSystemList[] = {
-    { QString("run_function"), QString("funcname"), _sysfunc_run_function },
+    { QString("runfunction"), QString("funcname"), _sysfunc_run_function },
 
     { QString(), QString(), NULL }
 };
@@ -22,5 +22,31 @@ finErrorCode finExecFunction::registSysFuncSystem()
 static finErrorCode _sysfunc_run_function(finExecFunction *self, finExecEnvironment *env,
                                           finExecMachine *machine, finExecFlowControl *flowctl)
 {
-    return finErrorCodeKits::FIN_EC_NON_IMPLEMENT;
+    finExecVariable *fnvar;
+
+    if ( self == NULL || env == NULL || machine == NULL || flowctl == NULL )
+        return finErrorCodeKits::FIN_EC_NULL_POINTER;
+
+    fnvar = env->findVariable("funcname")->getLinkTarget();
+    if ( fnvar == NULL )
+        return finErrorCodeKits::FIN_EC_NOT_FOUND;
+    QString funcname = fnvar->getStringValue();
+
+    finExecFunction *func = env->findFunction(funcname);
+    if ( func == NULL )
+        return finErrorCodeKits::FIN_EC_NOT_FOUND;
+
+    QList<finExecVariable *> arglist;
+    finExecVariable *argvar;
+    int idx = 0;
+    while ( true ) {
+        argvar = finExecFunction::getExtendArgAt(env, idx);
+        if ( argvar == NULL )
+            break;
+
+        arglist.append(argvar);
+        idx++;
+    }
+
+    return func->execFunction(&arglist, env, machine, flowctl);
 }
