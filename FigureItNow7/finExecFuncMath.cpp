2a90c588e79e07e565fa330993ce242f30830677
Yulong Yu <yuylong@acm.org>
2017-01-15 15:33:23 +0800

    Add math script function COT/CTG.

--- FigureItNow7/finExecFuncMath.cpp
+++ FigureItNow7/finExecFuncMath.cpp
@@ -14,12 +14,16 @@ static finErrorCode _sysfunc_cos(finExecFunction *self, finExecEnvironment *env,
                                  finExecMachine *machine, finExecFlowControl *flowctl);
 static finErrorCode _sysfunc_tan(finExecFunction *self, finExecEnvironment *env,
                                  finExecMachine *machine, finExecFlowControl *flowctl);
+static finErrorCode _sysfunc_cot(finExecFunction *self, finExecEnvironment *env,
+                                 finExecMachine *machine, finExecFlowControl *flowctl);
 
 static struct finExecSysFuncRegItem _finSysFuncMathList[] = {
     { QString("sin"), QString("rad"), _sysfunc_sin },
     { QString("cos"), QString("rad"), _sysfunc_cos },
     { QString("tan"), QString("rad"), _sysfunc_tan },
     { QString("tg"),  QString("rad"), _sysfunc_tan },
+    { QString("cot"), QString("rad"), _sysfunc_cot },
+    { QString("ctg"), QString("rad"), _sysfunc_cot },
 
     { QString(), QString(), NULL }
 };
@@ -105,6 +109,34 @@ static finErrorCode _sysfunc_tan(finExecFunction *self, finExecEnvironment *env,
 
     retvar->setType(finExecVariable::FIN_VR_TYPE_NUMERIC);
     retvar->setNumericValue(tan(rad->getNumericValue()));
+    retvar->setWriteProtected();
+    retvar->clearLeftValue();
+
+    flowctl->setFlowNext();
+    flowctl->setReturnVariable(retvar);
+    return finErrorCodeKits::FIN_EC_SUCCESS;
+}
+
+static finErrorCode _sysfunc_cot(finExecFunction *self, finExecEnvironment *env,
+                                 finExecMachine *machine, finExecFlowControl *flowctl)
+{
+    finExecVariable *rad, *retvar;
+
+    if ( self == NULL || env == NULL || machine == NULL || flowctl == NULL )
+        return finErrorCodeKits::FIN_EC_NULL_POINTER;
+
+    rad = finExecVariable::transLinkTarget(env->findVariable("rad"));
+    if ( rad == NULL )
+        return finErrorCodeKits::FIN_EC_NOT_FOUND;
+    if ( rad->getType() != finExecVariable::FIN_VR_TYPE_NUMERIC )
+        return finErrorCodeKits::FIN_EC_INVALID_PARAM;
+
+    retvar = new finExecVariable();
+    if ( retvar == NULL )
+        return finErrorCodeKits::FIN_EC_OUT_OF_MEMORY;
+
+    retvar->setType(finExecVariable::FIN_VR_TYPE_NUMERIC);
+    retvar->setNumericValue(1.0 / tan(rad->getNumericValue()));
     retvar->setWriteProtected();
     retvar->clearLeftValue();
 
