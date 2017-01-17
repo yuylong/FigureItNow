292c45bdab9a03e3427b9b48904c3a7492fa922c
Yulong Yu <yuylong@acm.org>
2017-01-17 15:06:56 +0800

    Add the algorithm to compute the 2D lines and the cross point.

--- FigureItNow7/finFigureAlg.cpp
+++ FigureItNow7/finFigureAlg.cpp
@@ -2,5 +2,24 @@
 
 finFigureAlg::finFigureAlg()
 {
-
+    /* Do Nothing */
 }
+
+finFigAlgLine2D finFigureAlg::line2DFromPoints(const QPointF &pt1, const QPointF &pt2)
+{
+    finFigAlgLine2D retline;
+    retline.a = pt1.y() - pt2.y();
+    retline.b = pt2.x() - pt1.x();
+    retline.c = pt1.x() * pt2.y() - pt2.x() * pt1.y();
+    return retline;
+}
+
+QPointF finFigureAlg::lineCrossPoint(const finFigAlgLine2D &line1, const finFigAlgLine2D &line2)
+{
+    QPointF retpt;
+    double p = line1.a * line2.b - line2.a * line1.b;
+    retpt.setX((line1.b * line2.c - line2.b * line1.c) / p);
+    retpt.setY((line2.a * line1.c - line1.a * line2.c) / p);
+    return retpt;
+}
+
--- FigureItNow7/finFigureAlg.h
+++ FigureItNow7/finFigureAlg.h
@@ -1,11 +1,20 @@
 #ifndef FINFIGUREALG_H
 #define FINFIGUREALG_H
 
+#include <QPointF>
+
+struct finFigAlgLine2D {
+    // f(x) = ax + by + c = 0
+    double a, b, c;
+};
 
 class finFigureAlg
 {
 public:
     finFigureAlg();
+
+    static finFigAlgLine2D line2DFromPoints(const QPointF &pt1, const QPointF &pt2);
+    static QPointF lineCrossPoint(const finFigAlgLine2D &line1, const finFigAlgLine2D &line2);
 };
 
 #endif // FINFIGUREALG_H
