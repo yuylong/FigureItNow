3d885a8a555f08b87469ccf1f9966d62dbe1266f
Yulong Yu <yuylong@acm.org>
2017-01-29 14:24:40 +0800

    Add the filling and applying methods for graph config dialog.

--- FigureItNow7/finUiGraphConfigDlg.cpp
+++ FigureItNow7/finUiGraphConfigDlg.cpp
@@ -1,14 +1,61 @@
 #include "finUiGraphConfigDlg.h"
 #include "ui_finUiGraphConfigDlg.h"
 
+#include <qmath.h>
+#include <QPointF>
+
 finUiGraphConfigDlg::finUiGraphConfigDlg(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::finUiGraphConfigDlg)
 {
+    this->_inFilling = true;
     ui->setupUi(this);
+    this->_whRatio = 4.0 / 3.0;
+    this->_origXPosRatio = 0.5;
+    this->_origYPosRatio = 0.5;
+    this->_inFilling = false;
 }
 
 finUiGraphConfigDlg::~finUiGraphConfigDlg()
 {
     delete ui;
+}
+
+static inline int _rad_to_deg(double rad)
+{
+    return (int)floor(rad * 180.0 / M_PI);
+}
+
+static inline double _deg_to_rad(int deg)
+{
+    return (double)deg * M_PI / 180.0;
+}
+
+void finUiGraphConfigDlg::fillFromGraphConfig(const finGraphConfig *graphcfg)
+{
+    this->_inFilling = true;
+    ui->lblBgColor->setColor(graphcfg->getBackgroundColor());
+
+    double pw = graphcfg->getPanelPixelWidth();
+    double ph = graphcfg->getPanelPixelHeight();
+    ui->spbPanelWidth->setValue((int)floor(pw));
+    ui->spbPanelHeight->setValue((int)floor(ph));
+    this->_whRatio = pw / ph;
+
+    QPointF origpt = graphcfg->getOriginPixelPoint();
+    ui->spbOriginPtX->setValue((int)floor(origpt.x()));
+    ui->spbOriginPtY->setValue((int)floor(origpt.y()));
+    this->_origXPosRatio = origpt.x() / pw;
+    this->_origYPosRatio = origpt.y() / ph;
+
+    ui->dsbAxisUnitSize->setValue(graphcfg->getAxisUnitPixelSize());
+    ui->spbAxisZDeg->setValue(_rad_to_deg(graphcfg->getAxisRadZ()));
+    ui->dsbAxisZRatio->setValue(graphcfg->getAxisScaleZ());
+
+    this->_inFilling = false;
+}
+
+void finUiGraphConfigDlg::applyToGraphConfig(finGraphConfig *graphcfg) const
+{
+
 }
--- FigureItNow7/finUiGraphConfigDlg.h
+++ FigureItNow7/finUiGraphConfigDlg.h
@@ -3,6 +3,8 @@
 
 #include <QDialog>
 
+#include "finGraphConfig.h"
+
 namespace Ui {
 class finUiGraphConfigDlg;
 }
@@ -11,9 +13,17 @@ class finUiGraphConfigDlg : public QDialog
 {
     Q_OBJECT
 
+protected:
+    bool _inFilling;
+    double _whRatio;
+    double _origXPosRatio, _origYPosRatio;
+
 public:
     explicit finUiGraphConfigDlg(QWidget *parent = 0);
     ~finUiGraphConfigDlg();
+
+    void fillFromGraphConfig(const finGraphConfig *graphcfg);
+    void applyToGraphConfig(finGraphConfig *graphcfg) const;
 
 private:
     Ui::finUiGraphConfigDlg *ui;
