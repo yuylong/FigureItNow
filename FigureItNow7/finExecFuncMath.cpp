0c8ccc8da5296cf6604347053506a0a4c8c9bc3c
Yulong Yu <yuylong@acm.org>
2017-02-03 11:09:59 +0800

    Add linear script math function.

--- FigureItNow7/finExecFuncMath.cpp
+++ FigureItNow7/finExecFuncMath.cpp
@@ -36,15 +36,22 @@ static finErrorCode _sysfunc_ln(finExecFunction *self, finExecEnvironment *env,
 static finErrorCode _sysfunc_log(finExecFunction *self, finExecEnvironment *env,
                                  finExecMachine *machine, finExecFlowControl *flowctl);
 
+static finErrorCode _sysfunc_linear(finExecFunction *self, finExecEnvironment *env,
+                                    finExecMachine *machine, finExecFlowControl *flowctl);
+static finErrorCode _sysfunc_quadratic(finExecFunction *self, finExecEnvironment *env,
+                                       finExecMachine *machine, finExecFlowControl *flowctl);
+
 static struct finExecSysFuncRegItem _finSysFuncMathList[] = {
-    { QString("sin"), QString("rad"),      _sysfunc_sin },
-    { QString("cos"), QString("rad"),      _sysfunc_cos },
-    { QString("tan"), QString("rad"),      _sysfunc_tan },
-    { QString("tg"),  QString("rad"),      _sysfunc_tan },
-    { QString("cot"), QString("rad"),      _sysfunc_cot },
-    { QString("ctg"), QString("rad"),      _sysfunc_cot },
-    { QString("ln"),  QString("base"),     _sysfunc_ln  },
-    { QString("log"), QString("idx,base"), _sysfunc_log },
+    { QString("sin"),    QString("rad"),      _sysfunc_sin    },
+    { QString("cos"),    QString("rad"),      _sysfunc_cos    },
+    { QString("tan"),    QString("rad"),      _sysfunc_tan    },
+    { QString("tg"),     QString("rad"),      _sysfunc_tan    },
+    { QString("cot"),    QString("rad"),      _sysfunc_cot    },
+    { QString("ctg"),    QString("rad"),      _sysfunc_cot    },
+    { QString("ln"),     QString("base"),     _sysfunc_ln     },
+    { QString("log"),    QString("idx,base"), _sysfunc_log    },
+
+    { QString("linear"), QString ("x,a,b"),   _sysfunc_linear },
 
     { QString(), QString(), NULL }
 };
@@ -224,3 +231,38 @@ static finErrorCode _sysfunc_log(finExecFunction *self, finExecEnvironment *env,
     return finErrorCodeKits::FIN_EC_SUCCESS;
 }
 
+static finErrorCode _sysfunc_linear(finExecFunction *self, finExecEnvironment *env,
+                                    finExecMachine *machine, finExecFlowControl *flowctl)
+{
+    finExecVariable *xvar, *avar, *bvar, *retvar;
+
+    if ( self == NULL || env == NULL || machine == NULL || flowctl == NULL )
+        return finErrorCodeKits::FIN_EC_NULL_POINTER;
+
+    xvar = finExecVariable::transLinkTarget(env->findVariable("x"));
+    avar = finExecVariable::transLinkTarget(env->findVariable("a"));
+    bvar = finExecVariable::transLinkTarget(env->findVariable("b"));
+    if ( xvar == NULL || avar == NULL || bvar == NULL )
+        return finErrorCodeKits::FIN_EC_NOT_FOUND;
+    if ( xvar->getType() != finExecVariable::FIN_VR_TYPE_NUMERIC ||
+         avar->getType() != finExecVariable::FIN_VR_TYPE_NUMERIC ||
+         bvar->getType() != finExecVariable::FIN_VR_TYPE_NUMERIC )
+        return finErrorCodeKits::FIN_EC_INVALID_PARAM;
+
+    double x = xvar->getNumericValue();
+    double a = avar->getNumericValue();
+    double b = bvar->getNumericValue();
+
+    retvar = new finExecVariable();
+    if ( retvar == NULL )
+        return finErrorCodeKits::FIN_EC_OUT_OF_MEMORY;
+
+    retvar->setType(finExecVariable::FIN_VR_TYPE_NUMERIC);
+    retvar->setNumericValue(a * x + b);
+    retvar->setWriteProtected();
+    retvar->clearLeftValue();
+
+    flowctl->setFlowNext();
+    flowctl->setReturnVariable(retvar);
+    return finErrorCodeKits::FIN_EC_SUCCESS;
+}
