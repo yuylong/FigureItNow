1eaebb8489653ba395463f97cdf5bde98c4eb7a2
Yulong Yu <yuylong@acm.org>
2017-02-04 14:12:06 +0800

    Add two variances of line and dot plotting.

--- FigureItNow7/finExecFuncPlot.cpp
+++ FigureItNow7/finExecFuncPlot.cpp
@@ -19,14 +19,26 @@
 
 static finErrorCode _sysfunc_plot_dots(finExecFunction *self, finExecEnvironment *env,
                                        finExecMachine *machine, finExecFlowControl *flowctl);
+static finErrorCode _sysfunc_plot_dots_xy(finExecFunction *self, finExecEnvironment *env,
+                                          finExecMachine *machine, finExecFlowControl *flowctl);
+static finErrorCode _sysfunc_plot_dots_mat(finExecFunction *self, finExecEnvironment *env,
+                                           finExecMachine *machine, finExecFlowControl *flowctl);
 static finErrorCode _sysfunc_plot_line(finExecFunction *self, finExecEnvironment *env,
                                        finExecMachine *machine, finExecFlowControl *flowctl);
+static finErrorCode _sysfunc_plot_line_xy(finExecFunction *self, finExecEnvironment *env,
+                                          finExecMachine *machine, finExecFlowControl *flowctl);
+static finErrorCode _sysfunc_plot_line_mat(finExecFunction *self, finExecEnvironment *env,
+                                           finExecMachine *machine, finExecFlowControl *flowctl);
 static finErrorCode _sysfunc_plot_function(finExecFunction *self, finExecEnvironment *env,
                                            finExecMachine *machine, finExecFlowControl *flowctl);
 
 static finExecSysFuncRegItem _finSysFuncPlotList[] = {
     { QString("plot_dots"),         QString("ary"),                  _sysfunc_plot_dots          },
+    { QString("plot_dots_xy"),      QString("xary,yary"),            _sysfunc_plot_dots_xy       },
+    { QString("plot_dots_mat"),     QString("mat"),                  _sysfunc_plot_dots_mat      },
     { QString("plot_line"),         QString("ary"),                  _sysfunc_plot_line          },
+    { QString("plot_line_xy"),      QString("xary,yary"),            _sysfunc_plot_line_xy       },
+    { QString("plot_line_mat"),     QString("mat"),                  _sysfunc_plot_line_mat      },
     { QString("plot_function"),     QString("x1,x2,func"),           _sysfunc_plot_function      },
 
     { QString(), QString(), NULL }
@@ -67,7 +79,7 @@ static finErrorCode _sysfunc_plot_help_readdots_xyarray(finPlotDots *plotdots,
     if ( plotdots == NULL )
         return finErrorCodeKits::FIN_EC_NULL_POINTER;
 
-    if ( (xary == NULL || xary->getType() == finExecVariable::FIN_VR_TYPE_NULL) &&
+    if ( (xary == NULL || xary->getType() == finExecVariable::FIN_VR_TYPE_NULL) ||
          (yary == NULL || yary->getType() == finExecVariable::FIN_VR_TYPE_NULL) ) {
         plotdots->clearPoints();
         return finErrorCodeKits::FIN_EC_SUCCESS;
@@ -116,7 +128,6 @@ static finErrorCode _sysfunc_plot_help_readdots_xymat(finPlotDots *plotdots, fin
     return finErrorCodeKits::FIN_EC_SUCCESS;
 }
 
-
 static finErrorCode _sysfunc_plot_dots(finExecFunction *self, finExecEnvironment *env,
                                        finExecMachine *machine, finExecFlowControl *flowctl)
 {
@@ -141,6 +152,55 @@ static finErrorCode _sysfunc_plot_dots(finExecFunction *self, finExecEnvironment
     return finErrorCodeKits::FIN_EC_SUCCESS;
 }
 
+static finErrorCode _sysfunc_plot_dots_xy(finExecFunction *self, finExecEnvironment *env,
+                                          finExecMachine *machine, finExecFlowControl *flowctl)
+{
+    if ( self == NULL || env == NULL || machine == NULL || flowctl == NULL )
+        return finErrorCodeKits::FIN_EC_NULL_POINTER;
+    if ( env->getFigureContainer() == NULL )
+        return finErrorCodeKits::FIN_EC_STATE_ERROR;
+
+    finExecVariable *xaryvar = finExecVariable::transLinkTarget(env->findVariable("xary"));
+    finExecVariable *yaryvar = finExecVariable::transLinkTarget(env->findVariable("yary"));
+
+    finPlotDots dotplot;
+    finErrorCode errcode = _sysfunc_plot_help_readdots_xyarray(&dotplot, xaryvar, yaryvar);
+    if ( finErrorCodeKits::isErrorResult(errcode) )
+        return errcode;
+
+    dotplot.setFigureContainer(env->getFigureContainer());
+    errcode = dotplot.plot();
+    if ( finErrorCodeKits::isErrorResult(errcode) )
+        return errcode;
+
+    flowctl->setFlowNext();
+    return finErrorCodeKits::FIN_EC_SUCCESS;
+}
+
+static finErrorCode _sysfunc_plot_dots_mat(finExecFunction *self, finExecEnvironment *env,
+                                           finExecMachine *machine, finExecFlowControl *flowctl)
+{
+    if ( self == NULL || env == NULL || machine == NULL || flowctl == NULL )
+        return finErrorCodeKits::FIN_EC_NULL_POINTER;
+    if ( env->getFigureContainer() == NULL )
+        return finErrorCodeKits::FIN_EC_STATE_ERROR;
+
+    finExecVariable *matvar = finExecVariable::transLinkTarget(env->findVariable("mat"));
+
+    finPlotDots dotplot;
+    finErrorCode errcode = _sysfunc_plot_help_readdots_xymat(&dotplot, matvar);
+    if ( finErrorCodeKits::isErrorResult(errcode) )
+        return errcode;
+
+    dotplot.setFigureContainer(env->getFigureContainer());
+    errcode = dotplot.plot();
+    if ( finErrorCodeKits::isErrorResult(errcode) )
+        return errcode;
+
+    flowctl->setFlowNext();
+    return finErrorCodeKits::FIN_EC_SUCCESS;
+}
+
 static finErrorCode _sysfunc_plot_line(finExecFunction *self, finExecEnvironment *env,
                                        finExecMachine *machine, finExecFlowControl *flowctl)
 {
@@ -151,13 +211,62 @@ static finErrorCode _sysfunc_plot_line(finExecFunction *self, finExecEnvironment
 
     finExecVariable *aryvar = finExecVariable::transLinkTarget(env->findVariable("ary"));
 
-    finPlotDotsLine dotplot;
-    finErrorCode errcode = _sysfunc_plot_help_readdots_array(&dotplot, aryvar);
+    finPlotDotsLine lnplot;
+    finErrorCode errcode = _sysfunc_plot_help_readdots_array(&lnplot, aryvar);
     if ( finErrorCodeKits::isErrorResult(errcode) )
         return errcode;
 
-    dotplot.setFigureContainer(env->getFigureContainer());
-    errcode = dotplot.plot();
+    lnplot.setFigureContainer(env->getFigureContainer());
+    errcode = lnplot.plot();
+    if ( finErrorCodeKits::isErrorResult(errcode) )
+        return errcode;
+
+    flowctl->setFlowNext();
+    return finErrorCodeKits::FIN_EC_SUCCESS;
+}
+
+static finErrorCode _sysfunc_plot_line_xy(finExecFunction *self, finExecEnvironment *env,
+                                          finExecMachine *machine, finExecFlowControl *flowctl)
+{
+    if ( self == NULL || env == NULL || machine == NULL || flowctl == NULL )
+        return finErrorCodeKits::FIN_EC_NULL_POINTER;
+    if ( env->getFigureContainer() == NULL )
+        return finErrorCodeKits::FIN_EC_STATE_ERROR;
+
+    finExecVariable *xaryvar = finExecVariable::transLinkTarget(env->findVariable("xary"));
+    finExecVariable *yaryvar = finExecVariable::transLinkTarget(env->findVariable("yary"));
+
+    finPlotDotsLine lnplot;
+    finErrorCode errcode = _sysfunc_plot_help_readdots_xyarray(&lnplot, xaryvar, yaryvar);
+    if ( finErrorCodeKits::isErrorResult(errcode) )
+        return errcode;
+
+    lnplot.setFigureContainer(env->getFigureContainer());
+    errcode = lnplot.plot();
+    if ( finErrorCodeKits::isErrorResult(errcode) )
+        return errcode;
+
+    flowctl->setFlowNext();
+    return finErrorCodeKits::FIN_EC_SUCCESS;
+}
+
+static finErrorCode _sysfunc_plot_line_mat(finExecFunction *self, finExecEnvironment *env,
+                                           finExecMachine *machine, finExecFlowControl *flowctl)
+{
+    if ( self == NULL || env == NULL || machine == NULL || flowctl == NULL )
+        return finErrorCodeKits::FIN_EC_NULL_POINTER;
+    if ( env->getFigureContainer() == NULL )
+        return finErrorCodeKits::FIN_EC_STATE_ERROR;
+
+    finExecVariable *matvar = finExecVariable::transLinkTarget(env->findVariable("mat"));
+
+    finPlotDotsLine lnplot;
+    finErrorCode errcode = _sysfunc_plot_help_readdots_xymat(&lnplot, matvar);
+    if ( finErrorCodeKits::isErrorResult(errcode) )
+        return errcode;
+
+    lnplot.setFigureContainer(env->getFigureContainer());
+    errcode = lnplot.plot();
     if ( finErrorCodeKits::isErrorResult(errcode) )
         return errcode;
 
