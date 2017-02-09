bc8cf684649a43929fae0286528a245a35c5200f
Yulong Yu <yuylong@acm.org>
2017-02-09 16:35:08 +0800

    Add SEC and CSC math script functions.

--- FigureItNow7/finExecFuncMath.cpp
+++ FigureItNow7/finExecFuncMath.cpp
@@ -32,6 +32,10 @@ static finErrorCode _sysfunc_tan(finExecFunction *self, finExecEnvironment *env,
                                  finExecMachine *machine, finExecFlowControl *flowctl);
 static finErrorCode _sysfunc_cot(finExecFunction *self, finExecEnvironment *env,
                                  finExecMachine *machine, finExecFlowControl *flowctl);
+static finErrorCode _sysfunc_sec(finExecFunction *self, finExecEnvironment *env,
+                                 finExecMachine *machine, finExecFlowControl *flowctl);
+static finErrorCode _sysfunc_csc(finExecFunction *self, finExecEnvironment *env,
+                                 finExecMachine *machine, finExecFlowControl *flowctl);
 static finErrorCode _sysfunc_ln(finExecFunction *self, finExecEnvironment *env,
                                 finExecMachine *machine, finExecFlowControl *flowctl);
 static finErrorCode _sysfunc_log(finExecFunction *self, finExecEnvironment *env,
@@ -75,6 +79,8 @@ static struct finExecSysFuncRegItem _finSysFuncMathList[] = {
     { QString("tg"),                        QString("rad"),              _sysfunc_tan                       },
     { QString("cot"),                       QString("rad"),              _sysfunc_cot                       },
     { QString("ctg"),                       QString("rad"),              _sysfunc_cot                       },
+    { QString("sec"),                       QString("rad"),              _sysfunc_sec                       },
+    { QString("csc"),                       QString("rad"),              _sysfunc_csc                       },
     { QString("ln"),                        QString("base"),             _sysfunc_ln                        },
     { QString("log"),                       QString("idx,base"),         _sysfunc_log                       },
 
@@ -207,6 +213,62 @@ static finErrorCode _sysfunc_cot(finExecFunction *self, finExecEnvironment *env,
 
     retvar->setType(finExecVariable::FIN_VR_TYPE_NUMERIC);
     retvar->setNumericValue(1.0 / tan(rad->getNumericValue()));
+    retvar->setWriteProtected();
+    retvar->clearLeftValue();
+
+    flowctl->setFlowNext();
+    flowctl->setReturnVariable(retvar);
+    return finErrorCodeKits::FIN_EC_SUCCESS;
+}
+
+static finErrorCode _sysfunc_sec(finExecFunction *self, finExecEnvironment *env,
+                                 finExecMachine *machine, finExecFlowControl *flowctl)
+{
+    finExecVariable *rad, *retvar;
+
+    if ( self == NULL || env == NULL || machine == NULL || flowctl == NULL )
+        return finErrorCodeKits::FIN_EC_NULL_POINTER;
+
+    rad = finExecVariable::transLinkTarget(env->findVariable("rad"));
+    if ( rad == NULL )
+        return finErrorCodeKits::FIN_EC_NOT_FOUND;
+    if ( rad->getType() != finExecVariable::FIN_VR_TYPE_NUMERIC )
+        return finErrorCodeKits::FIN_EC_INVALID_PARAM;
+
+    retvar = new finExecVariable();
+    if ( retvar == NULL )
+        return finErrorCodeKits::FIN_EC_OUT_OF_MEMORY;
+
+    retvar->setType(finExecVariable::FIN_VR_TYPE_NUMERIC);
+    retvar->setNumericValue(1.0 / cos(rad->getNumericValue()));
+    retvar->setWriteProtected();
+    retvar->clearLeftValue();
+
+    flowctl->setFlowNext();
+    flowctl->setReturnVariable(retvar);
+    return finErrorCodeKits::FIN_EC_SUCCESS;
+}
+
+static finErrorCode _sysfunc_csc(finExecFunction *self, finExecEnvironment *env,
+                                 finExecMachine *machine, finExecFlowControl *flowctl)
+{
+    finExecVariable *rad, *retvar;
+
+    if ( self == NULL || env == NULL || machine == NULL || flowctl == NULL )
+        return finErrorCodeKits::FIN_EC_NULL_POINTER;
+
+    rad = finExecVariable::transLinkTarget(env->findVariable("rad"));
+    if ( rad == NULL )
+        return finErrorCodeKits::FIN_EC_NOT_FOUND;
+    if ( rad->getType() != finExecVariable::FIN_VR_TYPE_NUMERIC )
+        return finErrorCodeKits::FIN_EC_INVALID_PARAM;
+
+    retvar = new finExecVariable();
+    if ( retvar == NULL )
+        return finErrorCodeKits::FIN_EC_OUT_OF_MEMORY;
+
+    retvar->setType(finExecVariable::FIN_VR_TYPE_NUMERIC);
+    retvar->setNumericValue(1.0 / sin(rad->getNumericValue()));
     retvar->setWriteProtected();
     retvar->clearLeftValue();
 
