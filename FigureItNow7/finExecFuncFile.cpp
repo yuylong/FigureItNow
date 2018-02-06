789ae348389af2c9e4642b32b7fe1e3a0b4eb629
Yulong Yu <yuylong@acm.org>
2018-02-06 14:13:06 +0800

    Add script function to load CSV file as string variables.
    load_string_csv(fn);

--- FigureItNow7/finExecFuncFile.cpp
+++ FigureItNow7/finExecFuncFile.cpp
@@ -21,10 +21,13 @@ static finErrorCode _sysfunc_load_image(finExecFunction *self, finExecEnvironmen
                                         finExecMachine *machine, finExecFlowControl *flowctl);
 static finErrorCode _sysfunc_load_numerical_csv(finExecFunction *self, finExecEnvironment *env,
                                                 finExecMachine *machine, finExecFlowControl *flowctl);
+static finErrorCode _sysfunc_load_string_csv(finExecFunction *self, finExecEnvironment *env,
+                                             finExecMachine *machine, finExecFlowControl *flowctl);
 
 static struct finExecSysFuncRegItem _finSysFuncFileList[] = {
     { QString("load_image"),         QString("fn"), _sysfunc_load_image         },
     { QString("load_numerical_csv"), QString("fn"), _sysfunc_load_numerical_csv },
+    { QString("load_string_csv"),    QString("fn"), _sysfunc_load_string_csv    },
 
     { QString(), QString(), NULL }
 };
@@ -113,6 +116,59 @@ static finErrorCode _sysfunc_load_numerical_csv(finExecFunction *self, finExecEn
 
             colvar->setType(finExecVariable::TP_NUMERIC);
             colvar->setNumericValue(valitem);
+            curcol++;
+        }
+        currow++;
+    }
+    retvar->setWriteProtected();
+    retvar->clearLeftValue();
+
+    flowctl->setFlowNext();
+    flowctl->setReturnVariable(retvar);
+    return finErrorKits::EC_SUCCESS;
+}
+
+static finErrorCode _sysfunc_load_string_csv(finExecFunction *self, finExecEnvironment *env,
+                                             finExecMachine *machine, finExecFlowControl *flowctl)
+{
+    finExecVariable *fnvar, *retvar;
+
+    if ( self == NULL || env == NULL || machine == NULL || flowctl == NULL )
+        return finErrorKits::EC_NULL_POINTER;
+
+    fnvar = finExecVariable::transLinkTarget(env->findVariable("fn"));
+    if ( fnvar == NULL )
+        return finErrorKits::EC_NOT_FOUND;
+    if ( fnvar->getType() != finExecVariable::TP_STRING )
+        return finErrorKits::EC_INVALID_PARAM;
+
+    retvar = new finExecVariable();
+    if ( retvar == NULL )
+        return finErrorKits::EC_OUT_OF_MEMORY;
+
+    QString filename = fnvar->getStringValue();
+    QFile fp(filename);
+    if ( !fp.open(QIODevice::ReadOnly | QIODevice::Text) )
+        return finErrorKits::EC_FILE_NOT_OPEN;
+
+    retvar->setType(finExecVariable::TP_ARRAY);
+    int currow = 0;
+    QTextStream tsin(&fp);
+    while ( !tsin.atEnd() ) {
+        QString lnstr = tsin.readLine();
+        QStringList lnstrlist = lnstr.split(',');
+
+        finExecVariable *rowvar = retvar->getVariableItemAt(currow);
+        rowvar->setType(finExecVariable::TP_ARRAY);
+        if ( lnstr.isEmpty() )
+            continue;
+
+        rowvar->preallocArrayLength(lnstrlist.length());
+        int curcol = 0;
+        foreach (QString stritem, lnstrlist) {
+            finExecVariable *colvar = rowvar->getVariableItemAt(curcol);
+            colvar->setType(finExecVariable::TP_STRING);
+            colvar->setStringValue(stritem);
             curcol++;
         }
         currow++;
