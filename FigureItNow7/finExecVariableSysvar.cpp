e2f1493f1714740cdee73073ebc2f349f67d2996
Yulong Yu <yuylong@acm.org>
2018-02-22 14:41:59 +0800

    Define some more constants.

--- FigureItNow7/finExecVariableSysvar.cpp
+++ FigureItNow7/finExecVariableSysvar.cpp
@@ -25,6 +25,17 @@ static finExecVariable *_sysvar_bernsteins_const();
 static finExecVariable *_sysvar_gauss_kuzmin_wirsing_const();
 static finExecVariable *_sysvar_hafner_sarnak_mccurley_const();
 static finExecVariable *_sysvar_landaus_const();
+static finExecVariable *_sysvar_omega_const();
+static finExecVariable *_sysvar_golomb_dickman_const();
+static finExecVariable *_sysvar_cahens_const();
+static finExecVariable *_sysvar_twin_prime_const();
+static finExecVariable *_sysvar_laplace_limit();
+static finExecVariable *_sysvar_embree_trefethen_const();
+static finExecVariable *_sysvar_landau_ramanujan_const();
+static finExecVariable *_sysvar_alladi_grinstead_const();
+static finExecVariable *_sysvar_prime_quad_bruns_const();
+static finExecVariable *_sysvar_catalans_const();
+static finExecVariable *_sysvar_legendres_const();
 
 typedef finExecVariable *(*_finSysvarGencall)();
 
@@ -41,6 +52,17 @@ _finSysvarGencall _finSysvarGencallList[] = {
     _sysvar_gauss_kuzmin_wirsing_const,
     _sysvar_hafner_sarnak_mccurley_const,
     _sysvar_landaus_const,
+    _sysvar_omega_const,
+    _sysvar_golomb_dickman_const,
+    _sysvar_cahens_const,
+    _sysvar_twin_prime_const,
+    _sysvar_laplace_limit,
+    _sysvar_embree_trefethen_const,
+    _sysvar_landau_ramanujan_const,
+    _sysvar_alladi_grinstead_const,
+    _sysvar_prime_quad_bruns_const,
+    _sysvar_catalans_const,
+    _sysvar_legendres_const,
     NULL
 };
 
@@ -196,4 +218,59 @@ static finExecVariable *_sysvar_hafner_sarnak_mccurley_const()
 static finExecVariable *_sysvar_landaus_const()
 {
     return _sysvar_gen_num_var(QString("LANDAUS_CONST"), 0.5);
+}
+
+static finExecVariable *_sysvar_omega_const()
+{
+    return _sysvar_gen_num_var(QString("OMEGA_CONST"), 0.56714329040978387299996866221035554);
+}
+
+static finExecVariable *_sysvar_golomb_dickman_const()
+{
+    return _sysvar_gen_num_var(QString("GOLOMB_DICKMAN_CONST"), 0.62432998854355087099293638310083724);
+}
+
+static finExecVariable *_sysvar_cahens_const()
+{
+    return _sysvar_gen_num_var(QString("CAHENS_CONST"), 0.6434105462);
+}
+
+static finExecVariable *_sysvar_twin_prime_const()
+{
+    return _sysvar_gen_num_var(QString("TWIN_PRIME_CONST"), 0.66016181584686957392781211001455577);
+}
+
+static finExecVariable *_sysvar_laplace_limit()
+{
+    return _sysvar_gen_num_var(QString("LAPLACE_LIMIT"), 0.66274341934918158097474209710925290);
+}
+
+static finExecVariable *_sysvar_embree_trefethen_const()
+{
+    return _sysvar_gen_num_var(QString("EMBREE_TREFETHEN_CONST"), 0.70258);
+}
+
+static finExecVariable *_sysvar_landau_ramanujan_const()
+{
+    return _sysvar_gen_num_var(QString("LANDAU_RAMANUJAN_CONST"), 0.76422365358922066299069873125009232);
+}
+
+static finExecVariable *_sysvar_alladi_grinstead_const()
+{
+    return _sysvar_gen_num_var(QString("ALLADI_GRINSTEAD_CONST"), 0.8093940205);
+}
+
+static finExecVariable *_sysvar_prime_quad_bruns_const()
+{
+    return _sysvar_gen_num_var(QString("PRIME_QUAD_BRUNS_CONST"), 0.87058838);
+}
+
+static finExecVariable *_sysvar_catalans_const()
+{
+    return _sysvar_gen_num_var(QString("CATALANS_CONST"), 0.91596559417721901505460351493238411);
+}
+
+static finExecVariable *_sysvar_legendres_const()
+{
+    return _sysvar_gen_num_var(QString("LEGENDRES_CONST"), 1.0);
 }
