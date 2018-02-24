d05ff5891b84f0c384e4b34329e34b0d6f6a0e52
Yulong Yu <yuylong@acm.org>
2018-02-24 15:43:12 +0800

    Add array sum script function.

--- FigureItNow7/finExecFuncMatrix.cpp
+++ FigureItNow7/finExecFuncMatrix.cpp
@@ -28,6 +28,8 @@ static finErrorCode _sysfunc_array_add(finExecFunction *self, finExecEnvironment
                                        finExecMachine *machine, finExecFlowControl *flowctl);
 static finErrorCode _sysfunc_array_sub(finExecFunction *self, finExecEnvironment *env,
                                        finExecMachine *machine, finExecFlowControl *flowctl);
+static finErrorCode _sysfunc_array_sum(finExecFunction *self, finExecEnvironment *env,
+                                       finExecMachine *machine, finExecFlowControl *flowctl);
 
 static finErrorCode _sysfunc_vec_dim(finExecFunction *self, finExecEnvironment *env,
                                      finExecMachine *machine, finExecFlowControl *flowctl);
@@ -62,6 +64,7 @@ static struct finExecSysFuncRegItem _finSysFuncMatrixList[] = {
     { QString("array_neg"),     QString("ary"),       _sysfunc_array_neg     },
     { QString("array_add"),     QString("ary1,ary2"), _sysfunc_array_add     },
     { QString("array_sub"),     QString("ary1,ary2"), _sysfunc_array_sub     },
+    { QString("array_sum"),     QString("ary"),       _sysfunc_array_sum     },
 
     { QString("vec_dim"),       QString("ary"),       _sysfunc_vec_dim       },
     { QString("vec_neg"),       QString("ary"),       _sysfunc_array_neg     },
@@ -331,6 +334,36 @@ static finErrorCode _sysfunc_array_sub(finExecFunction *self, finExecEnvironment
         return finErrorKits::EC_OUT_OF_MEMORY;
 
     errcode = finExecAlg::varArraySub(ary1var, ary2var, retvar);
+    if ( finErrorKits::isErrorResult(errcode) ) {
+        delete retvar;
+        return errcode;
+    }
+
+    retvar->clearLeftValue();
+    retvar->setWriteProtected();
+    flowctl->setFlowNext();
+    flowctl->setReturnVariable(retvar);
+    return finErrorKits::EC_SUCCESS;
+}
+
+static finErrorCode _sysfunc_array_sum(finExecFunction *self, finExecEnvironment *env,
+                                       finExecMachine *machine, finExecFlowControl *flowctl)
+{
+    finErrorCode errcode;
+    finExecVariable *aryvar;
+
+    if ( self == NULL || env == NULL || machine == NULL || flowctl == NULL )
+        return finErrorKits::EC_NULL_POINTER;
+
+    aryvar = finExecVariable::transLinkTarget(env->findVariable("ary"));
+    if ( aryvar == NULL )
+        return finErrorKits::EC_NOT_FOUND;
+
+    finExecVariable *retvar = new finExecVariable();
+    if ( retvar == NULL )
+        return finErrorKits::EC_OUT_OF_MEMORY;
+
+    errcode = finExecAlg::varArraySum(aryvar, retvar);
     if ( finErrorKits::isErrorResult(errcode) ) {
         delete retvar;
         return errcode;
